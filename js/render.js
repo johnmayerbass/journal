0fb58f3da7f0e6852b1655b39eedd63598c39a6b
greenhorn <berenbaum4@gmail.com>
2026-02-14 22:31:36 +0900

    n

--- js/render.js
+++ js/render.js
@@ -428,14 +428,14 @@ function renderBlogCategory() {
     if (evt.target === categoryButton) {
       categoryWrapper.classList.toggle("active");
       categoryTitle.classList.toggle("sr-only");
-      categoryContainer.classList.toggle("md:flex");
+      categoryContainer.classList.toggle("hidden");
     } else if (
       categoryWrapper.classList.contains("active") &&
       !categoryWrapper.contains(evt.target)
     ) {
       categoryWrapper.classList.remove("active");
       categoryTitle.classList.add("sr-only");
-      categoryContainer.classList.remove("md:flex");
+      categoryContainer.classList.remove("hidden");
     }
   });
 
